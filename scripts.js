2b9e1860cc576657f28c574604ece4c7ad79a5ee
bennettlopez00 <43563700+bennettlopez00@users.noreply.github.com>
2025-06-09 09:54:39 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,19 +1,19 @@
 // The URLs to block from
-const URLToBlock = "www.youtube.com";
+const URLToBlock = "youtube";
 // The query params that will be blocked
 const querySelectors = [
     "ytm-rich-section-renderer.rich-section-single-column",
     "ytm-reel-shelf-renderer",
-    // "ytd-rich-section-renderer.ytd-rich-grid-renderer"
 ];
-console.log(querySelectors)
+
+console.log(URLToBlock)
+console.log(window.location.host)
+console.log(window.location.hostname.includes(URLToBlock))
 
 // Only run on the correct URL
-if (window.location.hostname === URLToBlock) {
+if (window.location.hostname.includes(URLToBlock)) {
   // Create the Observer
   const observer = new MutationObserver((mutations, obs) => {
-
-    console.log(querySelectors)
     // Iterate over each query selector
     for (let i = 0; i < querySelectors.length; i++) {
       var currentQuerySelector = querySelectors[i];
